16f7c264b155352a631aba14953b36d493c00b7b
mochang2 <mochang2@gmail.com>
2025-11-02 12:41:17 +0900

    test: 공백 개수 스타일 수정

--- fixtures/ts/src/utils/Error.vue
+++ fixtures/ts/src/utils/Error.vue
@@ -12,7 +12,7 @@ const aa "fdf";
 // [TEST] normally not execute when there are any parsing errors
 // [TEST] normally not execute at the time when the parsing errors are fixed
 // [TEST] normally execute after the parsing errors are fixed
-// function zxcv() {
-//   console.log("zxcv called");
+// function newFunction() {
+//   console.log("newFunction called");
 // }
 </script>
